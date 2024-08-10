542f27efdcf0a5a191b61403674929255b8e5c74
Konrad Marcinkowski <km.dartmoor@gmail.com>
2024-08-10 13:52:53 +0200

    added comments

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,8 @@ fn grayscale_filter(_image: &DynamicImage, _name: &str) {
     let mut image_buffer: RgbaImage = ImageBuffer::new(_image.dimensions().0, _image.dimensions().1);
     let mut current_pixel_brightness: u8;
     for pixel in _image.pixels() {
+        //pixel: (X, Y, [R, G, B, A]);
+        //calculate pixel brightness, use it as color value to get grayscale effect
         current_pixel_brightness = (0.2126 * pixel.2.0[0] as f32 + 0.7152*pixel.2.0[1] as f32 + 0.0722*pixel.2.0[2] as f32) as u8;
         image_buffer[(pixel.0, pixel.1)] = Rgba([current_pixel_brightness, current_pixel_brightness, current_pixel_brightness, 255]);
     }
@@ -32,6 +34,8 @@ fn grayscale_filter(_image: &DynamicImage, _name: &str) {
 fn invert_image_filter(_image: &DynamicImage, _name: &str) {
     let mut image_buffer: RgbaImage = ImageBuffer::new(_image.dimensions().0, _image.dimensions().1);
     for pixel in _image.pixels() {
+        //pixel: (X, Y, [R, G, B, A]);
+        //subtract color value (R, G, B) from alpha value (A) per each pixel, set as color value 
         image_buffer[(pixel.0, pixel.1)] = Rgba([pixel.2[3] - pixel.2[0], pixel.2[3] - pixel.2[1], pixel.2[3] - pixel.2[2], pixel.2[3]]);
     }
     save_from_buffer(&image_buffer, &_name)
